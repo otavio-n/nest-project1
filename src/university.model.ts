4b81bd13921e38bbc641a6c67d4511da0dfe33ad
otavio-n <nishimuraotavio@gmail.com>
2022-07-25 14:22:39 -0300

    Add university model

---
+++ src/university.model.ts
@@ -0,0 +1,12 @@
+export class University {
+  _id: string;
+  name: string;
+  country: string;
+  state?: string;
+
+  constructor(_id: string, name: string, country: string) {
+    this._id = _id;
+    this.name = name;
+    this.country = country;
+  }
+}
